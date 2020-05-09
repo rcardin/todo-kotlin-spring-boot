885e8fb8d5b61470a76aabeae67d29f57fe31f51
rcardin <riccardo.cardin@gmail.com>
2020-05-09 21:33:37 +0200

    Added not found response to TaskHandler

--- src/main/kotlin/io/rcardin/todo/TaskHandler.kt
+++ src/main/kotlin/io/rcardin/todo/TaskHandler.kt
@@ -1,17 +1,44 @@
 package io.rcardin.todo
 
 import kotlinx.coroutines.reactive.asFlow
-import org.springframework.http.MediaType
+import kotlinx.coroutines.reactive.awaitFirstOrNull
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.*
 import reactor.core.publisher.Flux
+import java.net.URI
 
 @Component
 class TaskHandler(private val repository: TaskRepository) {
-    suspend fun findAllTasks(): ServerResponse {
+    suspend fun findAllTasks(request: ServerRequest): ServerResponse {
         val tasks: Flux<Task> = repository.findAll()
-        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
-                .bodyAndAwait(tasks.asFlow());
+        return ServerResponse
+                .ok()
+                .json()
+                .bodyAndAwait(tasks.asFlow())
     }
+
+    suspend fun findOne(request: ServerRequest): ServerResponse {
+        val id = request.pathVariable("id")
+        val task = repository.findById(id).awaitFirstOrNull()
+        return if (task != null) {
+            ServerResponse
+                    .ok()
+                    .json()
+                    .bodyValueAndAwait(task)
+        } else {
+            ServerResponse
+                    .notFound()
+                    .buildAndAwait()
+        }
+    }
+
+    /*
+    suspend fun save(request: ServerRequest): ServerResponse {
+        val task = request.bodyToMono(Task::class.java)
+                .flatMap { repository.save(it) }
+                .map { it.name }
+        return ServerResponse
+                .created(URI.create("/tasks/"))
+    }
+     */
 }
--- src/main/kotlin/io/rcardin/todo/TaskRouters.kt
+++ src/main/kotlin/io/rcardin/todo/TaskRouters.kt
@@ -2,9 +2,13 @@ package io.rcardin.todo
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.coRouter
 
 @Configuration
 class TaskRouters(private val handler: TaskHandler) {
     @Bean
-    fun taskRouter(): Nothing = TODO()
+    fun taskRouter() = coRouter {
+        GET("/tasks", handler::findAllTasks)
+        GET("tasks/{id}", handler::findOne)
+    }
 }
